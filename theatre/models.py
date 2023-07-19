5b1bb9ff5338e4b37d4b030496aaaa4aa36e5ca8
Andriy <an_mydel@gmail.com>
2023-07-19 10:43:23 +0300

    Add Actor, Genre and Play models

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="plays")
+    genres = models.ManyToManyField(Genre, related_name="plays")
+
+    def __str__(self):
+        return self.title
