5f545878b4d931af02552a89b15d674a87c27b2e
Andriy <an_mydel@gmail.com>
2023-07-20 19:54:57 +0300

    Write tests for Actor model

--- theatre/tests/test_models.py
+++ theatre/tests/test_models.py
@@ -1,13 +1,28 @@
 from django.test import TestCase
 
-from theatre.models import Genre
+from theatre.models import Genre, Actor
 
 
 class GenreModelTest(TestCase):
+    @classmethod
     def setUpTestData(cls):
         Genre.objects.create(name="drama")
 
-    def test_str_works_correctly(self):
+    def test_genre_str_works_correctly(self):
         genre = Genre.objects.get(id=1)
 
         self.assertEqual(str(genre), "drama")
+
+
+class ActorModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Actor.objects.create(
+            first_name="Adam",
+            last_name="Sandler"
+        )
+
+    def test_actor_str_works_correctly(self):
+        actor = Actor.objects.get(id=1)
+
+        self.assertEqual(str(actor), "Adam Sandler")
