f364a4156533f042551afc1b66bba1e5c687dd57
Andriy <an_mydel@gmail.com>
2023-07-21 21:29:36 +0300

    Write tests for Play model

--- theatre/tests/test_models.py
+++ theatre/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from theatre.models import Genre, Actor
+from theatre.models import Genre, Actor, Play
 
 
 class GenreModelTest(TestCase):
@@ -26,3 +26,17 @@ class ActorModelTest(TestCase):
         actor = Actor.objects.get(id=1)
 
         self.assertEqual(str(actor), "Adam Sandler")
+
+
+class PlayModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Play.objects.create(
+            title="Hamlet",
+            description="Best drama of Shakespeare"
+        )
+
+    def test_play_str_works_correctly(self):
+        play = Play.objects.get(id=1)
+
+        self.assertEqual(str(play), play.title)
