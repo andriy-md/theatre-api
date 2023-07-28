e925a22b5d3fe2c12b60142ae005ae97dd92a624
Andriy <an_mydel@gmail.com>
2023-07-28 18:44:56 +0300

    Add validation to TicketSerializer

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework import serializers
+from rest_framework import serializers, validators
+from rest_framework.exceptions import ValidationError
 
 from theatre.models import Genre, Actor, Play, TheatreHall, Reservation, Performance, Ticket
 
@@ -61,6 +62,29 @@ class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = ["id", "row", "seat", "performance", "reservation"]
+        validators = [
+            validators.UniqueTogetherValidator(
+                queryset=Ticket.objects.all(),
+                fields=("row", "seat", "performance"),
+                message="This seat has already been taken"
+            )
+        ]
+
+    def get_related_performance_object(self):
+        return Performance.objects.get(id=self.initial_data["performance"])
+
+    def validate_row(self, value):
+        performance = self.get_related_performance_object()
+        if not (1 <= value <= performance.theatre_hall.rows):
+            raise ValidationError(f"Row must be in range 1-{performance.theatre_hall.rows}")
+        return value
+
+    def validate_seat(self, value):
+        performance = self.get_related_performance_object()
+        if not (1 <= value <= performance.theatre_hall.seats_in_row):
+            raise ValidationError(f"Seat must be in range 1-{performance.theatre_hall.seats_in_row}")
+        return value
+
 
 
 class TicketListRetrieveSerializer(serializers.ModelSerializer):
