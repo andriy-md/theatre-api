a0025c76567f6afdd785725184ce265812e63266
Andriy <an_mydel@gmail.com>
2023-08-04 17:45:31 +0300

    Add Django Secret_Key to .env and make related changes

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-c28u6tazwnh59uvu!sbm#5ky49j036cz37gqh$l4tn9nwf@tc*"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-c28u6tazwnh59uvu!sbm#5ky49j036cz37gqh$l4tn9nwf@tc*"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
